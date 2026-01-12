ab1cbe6a0c08d4b42698a237f6587a6a6f51b326
Victor Eduardo <victoroliveiramkd@gmail.com>
2026-01-12 19:46:23 -0300

    fix: update homepage URL and adjust Vite base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/pf-barbearia-bladeebourbon/",
+      base: "/pf-barbearia-bladeebourbon",
       server: {
         open: true,
         port: 3000,
