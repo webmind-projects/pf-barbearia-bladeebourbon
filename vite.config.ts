9a5a2d42d2d11069cb3bd22400dec07067d233ca
Victor Eduardo <victoroliveiramkd@gmail.com>
2026-01-13 02:09:29 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig(({ mode }) => {
     return {
       base: "/pf-barbearia-bladeebourbon",
       server: {
-        open: true,
         port: 3000,
         host: '0.0.0.0',
       },
